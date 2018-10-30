91b66666cb23e814dbb8ab49e0069417e32fe05c
Badisi <badisi84@gmail.com>
2018-10-30 17:40:08 +0100

    refactor(ngx-safe-subscribe)

--- ngx-safe-subscribe.ts
+++ ngx-safe-subscribe.ts
@@ -1,19 +1,14 @@
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription } from "rxjs";
 
 declare module 'rxjs/internal/Observable' {
 	interface Observable<T> {
-		safeSubscribe(
-		    target: SafeSubscribable,
-		    next?: (value: T) => void,
-		    error?: (error: any) => void,
-		    complete?: () => void
-		): Subscription;
+        safeSubscribe: typeof safeSubscribe
   	}
 }
 
 export interface SafeSubscribable {
-  	_subscriptionFromSafeSubscribe$?: Subscription;
-	ngOnDestroy(): void;
+    _subscriptionFromSafeSubscribe$?: Subscription;
+ 	ngOnDestroy(): void;
 }
 
 export function safeSubscribe<T>(
@@ -22,23 +17,24 @@ export function safeSubscribe<T>(
     error?: (error: any) => void,
     complete?: () => void
 ): Subscription {
-    if( !('_subscriptionFromSafeSubscribe$' in target) ) {
-		target._subscriptionFromSafeSubscribe$ = new Subscription();
+	const sub = this.subscribe(next, error, complete);
+	if( target ) {
+	    if( !('_subscriptionFromSafeSubscribe$' in target) ) {
+			target._subscriptionFromSafeSubscribe$ = new Subscription();
 
-        const originalDestroy = target.ngOnDestroy;
-        if( !originalDestroy ) {
-            console.warn(`${(target as any).constructor.name} should implement OnDestroy otherwise Observable<T>.safeSubscribe will have no effect.`);
-        }
-        target.ngOnDestroy = function() {
-            if( originalDestroy && (typeof originalDestroy === 'function') ) {
-                originalDestroy.apply(this, arguments);
-            }
-            target._subscriptionFromSafeSubscribe$.unsubscribe();
-        };
-    }
-
-    const sub = this.subscribe(next, error, complete);
-    target._subscriptionFromSafeSubscribe$.add(sub);
+	        const originalDestroy = target.ngOnDestroy;
+	        if( !originalDestroy ) {
+	            console.warn(`${(target as any).constructor.name} should implement OnDestroy otherwise Observable<T>.safeSubscribe will have no effect.`);
+	        }
+	        target.ngOnDestroy = function() {
+	            if( originalDestroy && (typeof originalDestroy === 'function') ) {
+	                originalDestroy.apply(this, arguments);
+	            }
+	        	target._subscriptionFromSafeSubscribe$.unsubscribe();
+	        };
+	    }
+	    target._subscriptionFromSafeSubscribe$.add(sub);
+	}
     return sub;
 }
 Observable.prototype.safeSubscribe = safeSubscribe;
