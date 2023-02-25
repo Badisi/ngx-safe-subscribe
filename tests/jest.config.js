bb0a0763cd1fe8712a3f66b8255a62eec8702ce0
Badisi <badisi84@gmail.com>
2023-02-25 14:18:26 +0100

    fix(tests)

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
     globalSetup: 'jest-preset-angular/global-setup',
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.json'
-        }
+    transform: {
+        '\\.[jt]sx?$': ['jest-preset-angular', { tsconfig: '<rootDir>/tsconfig.json' }]
     }
 };
