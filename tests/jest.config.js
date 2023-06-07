ae47d88b91f026a49a918f89c00cd8559c6d8cc6
Badisi <badisi84@gmail.com>
2023-06-07 11:06:05 +0200

    refactor(tests): remove jest preset angular global setup as ngcc is no more needed

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    globalSetup: 'jest-preset-angular/global-setup',
     transform: {
         '\\.[jt]sx?$': ['jest-preset-angular', { tsconfig: '<rootDir>/tsconfig.json' }]
     }
